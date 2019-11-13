9b5aaeab118cf732ab0bedc1b69eb8e440fe8bc7
Justin O'Hara <justinrobertohara@gmail.com>
2019-11-12 22:28:58 -0800

    inital commitm

---
+++ front_end/Components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import $ from 'jquery';
+// import 'semantic-ui-popup/popup.css';
+
+// import 'semantic-ui-transition/transition.css';
+// $.fn.transition = require('semantic-ui-transition');
+
+export default class App extends React.Component {
+  render() {
+    return <div>trying to get semantic to work check</div>;
+  }
+}
