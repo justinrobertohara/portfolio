fc691941fcda4af37c30ffdf763d00bcd14b6b46
Justin O'Hara <justinrobertohara@gmail.com>
2020-04-17 20:16:01 -0700

    edit apr17

---
+++ front_end/Components/application.js
@@ -0,0 +1,32 @@
+<Jumbotron style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}>
+  <h1>Applications</h1>
+
+  <Card
+    style={{
+      backgroundColor: 'rgba(0, 0, 0, 0)',
+      border: '1px solid transparent',
+    }}
+    className="text-center p-3"
+  >
+    <Card.Text>Nike Microservice</Card.Text>
+    <a href="https://imgflip.com/gif/3o7goi">
+      <img
+        src="https://i.imgflip.com/3o7goi.gif"
+        title="made at imgflip.com"
+        // width="200"
+        // height="200"
+      />
+    </a>
+    <Card.Body>
+      Designed a responsive UI with Bootstrap for professional grade
+      responsiveness for a popular shoe brand item page. <br></br>• Deployed
+      microservice and proxy server to AWS EC2 instances <br></br>• Developed
+      RESTful API endpoints with express in order to streamline communication
+      between different deployed microservices on AWS <br></br>• Implemented TDD
+      using Jest and Enzyme to provide reliable and working software <br></br>•
+      Improved page load speeds by implementing Brotli and GZIP for compression
+      Javascript text files <br></br>• Designed a responsive UI with Bootstrap
+      for professional grade responsiveness
+    </Card.Body>
+  </Card>
+</Jumbotron>;
